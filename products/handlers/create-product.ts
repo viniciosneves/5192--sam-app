828dec592256fd480f9f8942906a28e9bb5644bb
Vinicios Neves <vinicios.neves@mac>
2025-12-01 17:00:28 +0000

    video 3.2

---
+++ products/handlers/create-product.ts
@@ -0,0 +1,22 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                path: event.path,
+                method: event.httpMethod,
+                fnName: 'products/handlers/create-product.ts',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
